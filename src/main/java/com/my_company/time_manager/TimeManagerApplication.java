0ce3e941156a9a3a0a3c3abee95847ef884277d3
Alex <53349658+AlexPavlenko91@users.noreply.github.com>
2021-06-02 16:01:13 +0300

    trying to connect custom css

--- src/main/java/com/my_company/time_manager/TimeManagerApplication.java
+++ src/main/java/com/my_company/time_manager/TimeManagerApplication.java
@@ -2,12 +2,35 @@ package com.my_company.time_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class TimeManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TimeManagerApplication.class, args);
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setTemplateMode("HTML");
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("CSS");
+        templateResolver.setPrefix("css/");
+        templateResolver.setSuffix(".css");
+
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver);
+
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(engine);
+        return viewResolver;
     }
 
 }
--- src/main/java/com/my_company/time_manager/config/MvcConfig.java
+++ src/main/java/com/my_company/time_manager/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.my_company.time_manager.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,8 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("show_users");
-        registry.addViewController("/login").setViewName("admin_login");
-        registry.addViewController("/registration").setViewName("admin_reg");
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/admin_login").setViewName("admin_login");
+        registry.addViewController("/admin/new_get").setViewName("admin_reg");
+        registry.addViewController("/user/new_get").setViewName("user_reg");
     }
+
+    /*@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+    }*/
 }
